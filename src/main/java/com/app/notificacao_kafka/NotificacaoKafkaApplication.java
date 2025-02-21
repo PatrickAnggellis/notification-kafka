5ae10703b652dc46e6e38cc1503b78d3033ab5b1
Patrick D Anggellis <patrickbananeiras@gmail.com>
2025-02-21 13:33:03 -0300

    Primeiro commit

---
+++ src/main/java/com/app/notificacao_kafka/NotificacaoKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.app.notificacao_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaoKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/notificacao_kafka/NotificacaoKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.notificacao_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
